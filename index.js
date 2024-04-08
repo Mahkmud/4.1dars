6cf2f302a37bf366ad390f9ba9e26d5bb71c64c9
Mahmudjon Shukurov <sukurovmahmudzon15@gmail.com>
2024-04-08 09:51:09 +0300

    tugadi

---
+++ index.js
@@ -0,0 +1,6 @@
+let ism = prompt("Ismingiz nima: ");
+let yosh = prompt("Yoshingizni kiriting");
+let adres = prompt("Adresingiz nima");
+let tugilgan_kun = ("Tugilgan kuningiz kacho");
+let nima_kasb = ("Nima kasb qiziqasiz");
+alert(ism +" " + yosh + "yosh -" + kasb +"!")
